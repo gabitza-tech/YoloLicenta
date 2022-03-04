67895119e27c6f9ccc4b7b42b9fdd0575105d373
gabitza-tech <53811655+gabitza-tech@users.noreply.github.com>
2022-03-04 16:58:29 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 from data.data import VOCdataset
 from data.transforms import GridTransform
-#from data.losses import YoloLoss
 from data.losses import YoloLoss
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -25,11 +24,18 @@ from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.python.keras.utils.vis_utils import plot_model
 from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.losses import MeanSquaredError
 from tensorflow.keras.metrics import mean_squared_error
+from tensorflow.keras.models import model_from_json
+import time
 #tf.enable_eager_execution()
 #tf.compat.v1.enable_eager_execution()
 #print(tf.executing_eagerly())
+
+#gpu_options = tf.GPUOptions(allow_growth=True)
+#session = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
+
 
 imageDir = 'dataset/images'
 annotDir = 'dataset/annotations'
@@ -51,7 +57,7 @@ data,bboxes,labels,no_objects, imagePaths = dataset.load_dataset(imageDir,annotD
 #default number of grids: no_grids =11 and default number of box predictions: B=2
 
 B = 2 # number of bbox predictions per cell
-no_grids = 9 # number of grid cells per image
+no_grids = 7 # number of grid cells per image
 GT = GridTransform(B, no_grids)
 
 bboxes_grids,labels_grids = GT.transform(data,bboxes,labels,no_objects)
@@ -60,7 +66,7 @@ bboxes_grids,labels_grids = GT.transform(data,bboxes,labels,no_objects)
 #print(bboxes_grids.shape)
 #print(labels_grids.shape)
 
-##############################
+"""
 ## FOR TWO HEADS NETOWRK SPLIT
 
 #bboxes_grids = np.reshape(bboxes_grids,(bboxes_grids.shape[0],bboxes_grids.shape[1]*bboxes_grids.shape[2]*bboxes_grids.shape[3]))
@@ -77,8 +83,7 @@ bboxes_grids,labels_grids = GT.transform(data,bboxes,labels,no_objects)
 #print(trainImages.shape)
 #print(valImages.shape)
 #print(testImages.shape)
-###############################
-
+"""
 ###############################
 ## IF I WANT TO USE ONLY ONE HEAD IN THE NETWORK
 
@@ -95,29 +100,24 @@ one_head_labels = np.reshape(one_head_labels,(-1,(len(classes)+B*5)*no_grids*no_
 trainImages, testImages, trainLabels, testLabels, trainPaths, testPaths = train_test_split(data, one_head_labels, imagePaths, test_size = 0.1, random_state=42)
 trainImages, valImages, trainLabels, valLabels = train_test_split(trainImages, trainLabels, test_size = 0.2, random_state=42)
 
+"""
 #trainImagesdataset = tf.data.Dataset.from_tensor_slices(
 #    (trainImages,trainLabels)
 #)
-
 #trainImagesdataset.batch(batch_size=32)
-
-
-
 #valImagesdataset = tf.data.Dataset.from_tensor_slices(
 #    (valImages,valLabels)
 #)
 #valImagesdataset.batch(batch_size=32)
-
 #testImagesdataset = tf.data.Dataset.from_tensor_slices(
 #    (testImages,testLabels)
 #)
 #testImagesdataset.batch(batch_size=32)
-
 #print(trainLabels.shape)
 #print(valLabels.shape)
-#print(testLabels.shape)
+print(testLabels.shape)
+"""
 
-###############################
 
 # write the testing image paths to disk so that we can use then
 # when evaluating/testing our object detector
@@ -129,42 +129,53 @@ f.close()
 model = ResNet50(include_top=False,weights='imagenet', input_shape=(224,224,3))
 flatten = Flatten()(model.output)
 
-head = Dense(4096,name='fc_1',activation = 'elu')(flatten)
-#head = BatchNormalization(name='batchbox1')(head)
-head = Dropout(0.5)(head)
-#head = Dense(1024,name='fc_2',activation = 'elu')(head)
-#head = BatchNormalization(name='batchbox2')(head)
-#head = Dropout(0.5)(head)
+for layer in model.layers:
+    layer.trainable=False
+for layer in model.layers:
+    print(layer,layer.trainable)
+
+head = Dense(2048,name='fc_1')(flatten)
+head = LeakyReLU(alpha=0.1)(head)
+#head = BatchNormalization()(head)
+head = Dropout(0.6)(head)
 head = Dense((len(classes)+B*5)*no_grids*no_grids, activation = 'sigmoid', name = 'fc_3')(head)#(len(classes)+B*5)*no_grids*no_grids
 
 detector = Model(inputs=model.input,outputs = head)
 plot_model(detector, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-"""
+
 #def step_decay(epoch):
     # initialize the base initial learning rate, drop factor, and
     # epochs to drop every
-    #initAlpha = 0.001
+    #initAlpha = 0.00001
     #factor = 0.5
-    #dropEvery = 20
+    #dropEvery = 60
 
     # compute learning rate for the current epoch
-    alpha = initAlpha * (factor ** np.floor((1 + epoch) / dropEvery))
-
+    #alpha = initAlpha * (factor ** np.floor((1 + epoch) / dropEvery))
+    #if epoch > 30 and epoch <60:
+    #    alpha = 0.0001
+    #elif epoch > 59 and epoch <100:
+    #    alpha = 0.00001
+    #else: 
+    #    alpha = 0.000001
+    
     # return the learning rate
-    return float(alpha)
-"""
+    #return float(alpha)
+
 
 fname = os.path.sep.join(["output/weights-best.hdf5"])
 checkpoint = ModelCheckpoint(fname, monitor="val_loss", save_best_only=True, mode = "min", verbose=1)
+tensorboardname = "Pascal-model-{}".format(int(time.time()))
+tensorboard = TensorBoard(log_dir='logs/{}'.format(tensorboardname))
 
-callbacks = [checkpoint]
+callbacks = [checkpoint,tensorboard]#,LearningRateScheduler(step_decay)
 
 loss = YoloLoss()
 #loss = MeanSquaredError(reduction="sum")
 loss.__name__ = "YoloLoss"
 # initialize the optimizer, compile the model, and show the model
 # summary
-opt = Adam(learning_rate=0.001)
+opt = Adam(learning_rate=0.000001)
 detector.compile(loss=loss, optimizer=opt, metrics=["mae"])#'mean_squared_error'
 
 # train the network for bounding box regression and class label
@@ -174,9 +185,18 @@ print("[INFO] training model...")
 H = detector.fit(
 	trainImages,trainLabels,
 	validation_data =(valImages,valLabels),
-	batch_size=32,
-	epochs=80,
+	batch_size=8,
+	epochs=120,
     callbacks = callbacks,
 	verbose=1)
 
 #loss = loss.forward(y_pred, y_true)
+
+
+# serialize model to JSON
+#model_json = detector.to_json()
+#with open("output/experiment.json", "w") as json_file:
+#    json_file.write(model_json)
+# serialize weights to HDF5
+#detector.save_weights("output/experiment.h5")
+#print("Saved model to disk")
