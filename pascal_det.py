7aff2cfd23c6f6d2c10aef71dd8ecd4d8b87ed35
gabitza-tech <53811655+gabitza-tech@users.noreply.github.com>
2022-06-27 16:10:17 +0300

    Create pascal_det.py

---
+++ pascal_det.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import rospy 
+import matplotlib.pyplot as pyplot
+import numpy as np 
+import sys
+
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.models import load_model
+from data.transforms import GridTransform
+from data.losses import YoloLoss
+
+from std_msgs.msg import String
+
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+import cv2
+model = load_model('models/7x7epoch250loss3p37.hdf5', custom_objects = {"YoloLoss":YoloLoss})
+
+bridge = CvBridge()
+cnt = 0
+
+no_grids=7
+B=2
+GT = GridTransform(B,no_grids)
+
+def imgmsg_to_cv2(msg):
+    #print(img_msg)
+    #if img_msg.encoding != "rgb8":
+    #    rospy.logerr("This Coral detect node has been hardcoded to the 'bgr8' encoding.  Come change the code if you're actually trying to implement a new camera")
+    #dtype = np.dtype("uint8") # Hardcode to 8 bits...
+    #dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
+    #image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), # and three channels of data. Since OpenCV works with bgr natively, we don't need to reorder the channels.
+    #                dtype=dtype, buffer=img_msg.data)
+    # If the byt order is different between the message and the system.
+    #if img_msg.is_bigendian == (sys.byteorder == 'little'):
+    #    image_opencv = image_opencv.byteswap().newbyteorder()
+    
+    #cv2.imwrite("saved_images/save.jpg",image_opencv)
+    #image = cv2.resize(image_opencv,(224,224), interpolation=cv2.INTER_AREA)
+    #image = img_to_array(image) / 255.0
+    #image = np.expand_dims(image, axis=0)
+    global cnt
+    # Convert your ROS Image message to OpenCV2
+    cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    (h_init,w_init) = cv2_img.shape[:2]
+    image = cv2.resize(cv2_img, (224,224), interpolation = cv2.INTER_AREA)
+    image = img_to_array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    
+    # predict the bounding box of the object along with the class label
+    prediction = model.predict(image)
+    #print(prediction.shape)
+    print("predicted")
+    cnt = cnt +1 
+    prediction = np.reshape(prediction[0],(30,no_grids,no_grids))
+    #print(prediction.shape)
+
+    boxPred = prediction[20:30,...]       
+    classPred = prediction[:20,...]
+    #print(boxPred.shape)
+    #print(boxPred[0,...])
+    #print(boxPred[5])
+    output_path = 'output/test_nms'
+    image_final = GT.transform_with_nms(boxPred,classPred,cv2_img)
+
+    image_to_ros = cv2.resize(image_final,(w_init,h_init), interpolation = cv2.INTER_AREA)
+    ret_msg = bridge.cv2_to_imgmsg(image_to_ros,encoding="bgr8")
+
+    pubDetections.publish(ret_msg)
+    # Save your OpenCV2 image as a jpeg
+    cv2.imwrite('saved_images/camera_new_image_{}.jpeg'.format(cnt), image_final)
+    
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('image_listener')
+
+    # Define your image topic
+    #image_topic = "/slower_image_raw"
+    image_topic = "/xtion/rgb/image_raw"
+
+    # Set up publishers
+    #pubFaceFound = rospy.Publisher('/face_found', String, queue_size = 10)
+    pubDetections = rospy.Publisher('/yolo_detections', Image, queue_size = 10)
+    
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, imgmsg_to_cv2)
+
+    # Spin until ctrl + c
+    rospy.spin()
