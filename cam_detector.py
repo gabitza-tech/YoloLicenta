8cbf555685fc19a50039eef23955be0b05fe4c24
gabitza-tech <53811655+gabitza-tech@users.noreply.github.com>
2022-06-27 15:54:53 +0300

    Update cam_detector.py

--- cam_detector.py
+++ cam_detector.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import load_model
 from data.data import VOCdataset
 from data.transforms import GridTransform
 from data.losses import YoloLoss
+from data.yolo_loss import yolo_loss
 import numpy as np
 import mimetypes
 import argparse
@@ -27,13 +28,14 @@ classes = ['person' , 'bird', 'cat', 'cow',
 
  
 print("Load trained model")
+#put you own model here with your custom objects. If trained with yolo_loss and mAP function, add them in custom objects
 model = load_model('output/7x7epoch250loss3p37.hdf5', custom_objects = {"YoloLoss":YoloLoss})
 
 no_grids=7
 B=2
 GT = GridTransform(B,no_grids)
 
-cap = cv2.VideoCapture(-1)
+cap = cv2.VideoCapture(0)
 
 # Check if the webcam is opened correctly
 if not cap.isOpened():
@@ -41,27 +43,24 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
-    image = cv2.resize(frame,(224,224), interpolation=cv2.INTER_AREA)
-
-    image = img_to_array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
+    image = cv2.resize(frame,(224,224), interpolation=cv2.INTER_AREA) 
+    image_p = img_to_array(image) / 255.0
+    image_p = np.expand_dims(image, axis=0)
 
     # predict the bounding box of the object along with the class label
-    prediction = model.predict(image)
+    prediction = model.predict(image_p)
     #print(prediction.shape)
-    prediction = np.reshape(prediction[0],(30,no_grids,no_grids))
+    prediction = np.reshape(prediction[0],(30,no_grids*no_grids))
     #print(prediction.shape)
 
     boxPred = prediction[20:30,...]       
     classPred = prediction[:20,...]
-    #print(boxPred.shape)
-    #print(boxPred[0,...])
-    #print(boxPred[5])
-    (h, w) = frame.shape[:2]
-    #GT.transform_from_grid(boxPred,classPred,h,w,image1)
-    GT.transform_with_nms(boxPred,classPred,h,w,image)
 
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-    #        break
+    (h, w) = frame.shape[:2]
+    
+    image = GT.transform_with_nms(boxPred,classPred,image)
+    cv2.imshow('Video', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 cap.release()
 cv2.destroyAllWindows()
